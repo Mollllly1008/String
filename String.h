192d4300af0ebb0d73747a8d88c76f18cbc5ccb0
zmdpbbt <165140229+zmdpbbt@users.noreply.github.com>
2024-10-05 11:43:19 +0800

    Update String.h

--- String.h
+++ String.h
@@ -7,7 +7,13 @@ using namespace std;
 class String
 {
 public:
-	
+	String(const char *s="");						// 构造函数
+	String(const String &Str, int pos, int n);
+	String(const char *s, int n);
+	String(int n, char c);
+	String(const String &Str);						// 拷贝构造函数
+	String & operator=(const String &Str);			// 必须为成员函数
+	virtual ~String();								// 析构函数（虚函数）
 
 private:
 	char* str;
